6b7aec3a804e98356116317e60c323951fc94cd3
Chisom Maxwell <chiscomax2013@gmail.com>
2023-08-04 17:28:19 +0100

    Mod Delete

--- src/screens/Customers/Account/fragments/DeleteAccountBtn.tsx
+++ src/screens/Customers/Account/fragments/DeleteAccountBtn.tsx
@@ -3,10 +3,13 @@ import AppBtn from "../../../../../components/AppBtn";
 import { useNavigation } from "@react-navigation/native";
 import routes, { AppNavProps } from "../../../../config/routes.config";
 import { useDeleteAccount } from "../../../../api/queries/account.queries";
+import { useState } from 'react';
+import ConfirmDialog from "../../../../../components/ConfirmDialog";
 
 
 export default function DeleteAccountBtn() {
      const queryClient = useQueryClient();
+     const [showConfirmDelete, setShowConfirmDelete] = useState(false);
      const navigation = useNavigation<AppNavProps>();
      const { mutate, isLoading } = useDeleteAccount({
           onSuccess(data) {
@@ -20,12 +23,19 @@ export default function DeleteAccountBtn() {
      })
 
      return (
-          <AppBtn
-               onPress={() => mutate(null)}
-               isLoading={isLoading}
-               textColor="red.500"
-               backgroundColor={"rgba(255,0,0,0.1)"}
-          >Delete Account</AppBtn>
+          <>
+               <AppBtn
+                    onPress={() => setShowConfirmDelete(true)}
+                    isLoading={isLoading}
+                    textColor="red.500"
+                    backgroundColor={"rgba(255,0,0,0.1)"}
+               >Delete Account</AppBtn>
+               <ConfirmDialog onConfirm={() => {
+                    setShowConfirmDelete(false);
+                    mutate(null);
+
+               }} message="Once you delete your account, you will lose all personal data associated to your account." title="Delete Account ??" onClose={() => setShowConfirmDelete(false)} isOpen={showConfirmDelete} />
+          </>
      )
 
 }
